8382cd52c68d07e0e371b8df9334ce66895bfc2b
Jukyeom <67585661+jukyeom@users.noreply.github.com>
2020-08-22 16:12:31 +0900

    Update Athlete_Sort.py

--- Week6/Athlete_Sort.py
+++ Week6/Athlete_Sort.py
@@ -4,4 +4,4 @@ for _ in range(N):
     athlete.append(list(map(int, input().split())))
 athlete.sort(key = lambda attribute : attribute[int(input())])
 for i in range(len(athlete)):
-    print(" ".join(athlete[i]), end = ' ')
+    print(" ".join(athlete[i]), end = '')
